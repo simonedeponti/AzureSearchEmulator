209bb7bfe90b4d76f1c5a3b9529f4b065e4ed84f
Simone Deponti <simone.deponti@gmail.com>
2017-07-01 00:39:17 +0200

    Updated tools to auto-create index

--- AzureSearchEmulator/tools.py
+++ AzureSearchEmulator/tools.py
@@ -11,37 +11,106 @@ URL_TEMPLATES = {
     'create': (
         '{solr_url}/admin/cores?'
         'action=CREATE&name={index}&'
+        'instanceDir=%2Fopt%2Fsolr%2Fserver%2Fsolr%2Fmycores%2F{index}&'
         'configSet=data_driven_schema_configs'
     ),
     'putschema': '{solr_url}/{index}/schema'
+}
+
+TYPES = {
+    'Edm.String': lambda tags: {
+        'type': 'text_general' if 'searchable' in tags else 'string'
+    },
+    'Collection(Edm.String)': lambda tags: {
+        'type': 'text_general' if 'searchable' in tags else 'string',
+        'multiValued': True
+    },
+    'Edm.Int32': lambda tags: {
+        'type': 'int'
+    },
+    'Edm.Int64': lambda tags: {
+        'type': 'long'
+    },
+    'Edm.Boolean': lambda tags: {
+        'type': 'boolean'
+    },
+    'Edm.Double': lambda tags: {
+        'type': 'double'
+    },
+    'Edm.DateTimeOffset': lambda tags: {
+        'type': 'date'
+    }
 }
 
 logger = getLogger(__name__)
 
 
 async def get_cores_status(client):
-    url = URL_TEMPLATES['status'].format(solr_url=SOLR_URL)
+    url = URL_TEMPLATES['status'].format(solr_url=SOLR_URL.rstrip('/'))
     cores = set()
     async with client.get(url) as resp:
-        status = fromstring(await resp.text()).find("//lst[@name='status']")
+        status = fromstring(await resp.text()).find("./lst[@name='status']")
+        if not status:
+            status = []
         for core in status:
             cores.add(core.attrib['name'])
         return cores
 
 
 async def create_core(client, name):
-    url = URL_TEMPLATES['create'].format(solr_url=SOLR_URL, index=name)
+    url = URL_TEMPLATES['create'].format(
+        solr_url=SOLR_URL.rstrip('/'),
+        index=name
+    )
+    logger.debug('Calling GET {}'.format(url))
     async with client.get(url) as resp:
-        if resp.status == 200:
-            return True
-        else:
-            return False
+        logger.debug(await resp.text())
+        return resp.status == 200
+
+
+def schema_to_solrops(schema):
+    ops = {
+        'add-field': []
+    }
+    for field_id, field_def in schema.items():
+        if field_def.get('is_primary', False):
+            if field_id != 'id':
+                ops['add-copy-field'] = {
+                    'source': field_id,
+                    'dest': 'id'
+                }
+            else:
+                continue
+        rule = {
+            'name': field_id,
+            'indexed': True
+        }
+        rule.update(
+            TYPES[field_def['type']](
+                field_def.get('tags', [])
+            )
+        )
+        if 'retrievable' in field_def['tags']:
+            rule['stored'] = True
+        ops['add-field'].append(rule)
+    return ops
+
+
+async def create_schema(client, index, operations):
+    url = URL_TEMPLATES['putschema'].format(
+        solr_url=SOLR_URL.rstrip('/'),
+        index=index
+    )
+    logger.debug('Calling POST {}'.format(url))
+    async with client.post(url, json=operations) as resp:
+        logger.debug(await resp.text())
+        return resp.status == 200
 
 
 async def main(loop, indexes):
     async with aiohttp.ClientSession(loop=loop) as client:
         existing_cores = await get_cores_status(client)
-        for index, definiton in indexes.items():
+        for index, definition in indexes.items():
             if index not in existing_cores:
                 logger.info("Creating core {}".format(index))
                 created = await create_core(client, index)
@@ -49,7 +118,14 @@ async def main(loop, indexes):
                     logger.critical("Failed to create core {}".format(index))
                 else:
                     logger.critical("Created core {}".format(index))
-
+                    operations = schema_to_solrops(definition['schema'])
+                    created = await create_schema(client, index, operations)
+                    if created:
+                        logger.info("Updated schema for {}".format(index))
+                    else:
+                        logger.critical(
+                            "Failed to update schema for {}".format(index)
+                        )
 
 
 def recreate_indexes(stream):
--- fixselinux.sh
+++ fixselinux.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo chcon -R -u system_u -t svirt_sandbox_file_t -l s0 AzureSearchEmulator
+sudo chcon -u system_u -t svirt_sandbox_file_t -l s0 example-indexes.json
